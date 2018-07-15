f8f26e7ea71b6c6b66ded47b4a5e3516b0f54c4a
jul-ia <julie78sd@gmail.com>
2018-07-15 12:08:56 +0300

    test1

---
+++ dz1_threads/dz1_threads/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz1_threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
